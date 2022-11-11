404bb7aa651d23151b3a8394ed8d3525d59b3c77
Abi Kothapalli <abi.kothapalli@gmail.com>
2022-11-10 18:45:35 -0600

    Prettier stuff

--- models/messages.js
+++ models/messages.js
@@ -1,21 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const messagesSchema = new Schema(
-  {
-    message: {
-      type: String,
-      required: true,
-    },
-    sender: {
-      type: Schema.Types.ObjectId,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+	{
+		message: {
+			type: String,
+			required: true,
+		},
+		sender: {
+			type: Schema.Types.ObjectId,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 // Prevent recompilation of model if it already exists
-export default mongoose.models.Messages || mongoose.model("Messages", messagesSchema);
+export default mongoose.models.Messages || mongoose.model('Messages', messagesSchema);
--- models/posts.js
+++ models/posts.js
@@ -1,25 +1,25 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const postsSchema = new Schema(
-    {
-        poster: {
-            type: Schema.Types.ObjectId,
-            required: true,
-        },
-        text: {
-            type: String,
-            required: true,
-        },
-        replies: {
-            type: [Schema.Types.ObjectId],
-            required: false,
-        },
-    },
-    {
-        timestamps: true,
-    }
+	{
+		poster: {
+			type: Schema.Types.ObjectId,
+			required: true,
+		},
+		text: {
+			type: String,
+			required: true,
+		},
+		replies: {
+			type: [Schema.Types.ObjectId],
+			required: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 // Prevent recompilation of model if it already exists
-export default mongoose.models.Posts || mongoose.model("Posts", postsSchema);
+export default mongoose.models.Posts || mongoose.model('Posts', postsSchema);
