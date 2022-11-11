e66ab4a4e959d190840e147de8ca40ccb09d64ad
Abi Kothapalli <abi.kothapalli@gmail.com>
2022-11-10 18:53:14 -0600

    Fixed prettier

--- models/volunteers.js
+++ models/volunteers.js
@@ -1,34 +1,35 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const volunteerSchema = new mongoose.Schema(
-  {
-    name: {
-      required: true,
-      type: String,
-    },
-    location: {
-      required: true,
-      type: String,
-    },
-    phone: {
-      required: true,
-      type: Integer,
-    },
-    pronouns: {
-      required: true,
-      type: String,
-    },
-    picture: {
-      required: false,
-      type: String,
-    },
-    admintag: {
-      required: false,
-      type: Integer,
-    },
-  },
-  { timestamps: true }
+const volunteersSchema = new mongoose.Schema(
+	{
+		name: {
+			required: true,
+			type: String,
+		},
+		location: {
+			required: true,
+			type: String,
+		},
+		phone: {
+			required: true,
+			type: Integer,
+		},
+		pronouns: {
+			required: true,
+			type: String,
+		},
+		picture: {
+			required: false,
+			type: String,
+		},
+		admintag: {
+			required: false,
+			type: Integer,
+		},
+	},
+	{ timestamps: true }
 );
 
-module.exports = mongoose.model("Volunteer", volunteerSchema);
+// Prevent recompilation of model if it already exists
+export default mongoose.models.Volunteers || mongoose.model('Volunteers', volunteersSchema);
