7bd4b96462efdc538af93d3a314e2dc46cc9d47e
Abi Kothapalli <abi.kothapalli@gmail.com>
2022-11-10 18:30:05 -0600

    Add models for messages and posts

---
+++ models/posts.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const postsSchema = new Schema(
+    {
+        poster: {
+            type: Schema.Types.ObjectId,
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        replies: {
+            type: [Schema.Types.ObjectId],
+            required: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Prevent recompilation of model if it already exists
+export default mongoose.models.Posts || mongoose.model("Posts", postsSchema);
