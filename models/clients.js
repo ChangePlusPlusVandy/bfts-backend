b9234e8e14cdd0f9184b4033a61fdb45eb3ea09b
Abi Kothapalli <abi.kothapalli@gmail.com>
2022-11-15 18:39:04 -0600

    Added model for clients

---
+++ models/clients.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const clientsSchema = new Schema(
+	{
+		name: {
+			required: true,
+			type: String,
+		},
+		pronouns: {
+			required: true,
+			type: String,
+		},
+		birthday: {
+			required: true,
+			type: Date,
+		},
+		location: {
+			required: true,
+			type: String,
+		},
+		livingSituation: {
+			required: true,
+			type: String,
+		},
+		background: {
+			required: true,
+			type: String,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+// Prevent recompilation of model if it already exists
+export default mongoose.models.clients || mongoose.model('clients', clientsSchema);
