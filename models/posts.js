4d5863734adbf42d07381cf6980beb48f68e2509
Abi Kothapalli <abi.kothapalli@gmail.com>
2022-11-29 18:41:26 -0600

    Update model for posts to enable replies and reactions

--- models/posts.js
+++ models/posts.js
@@ -7,14 +7,26 @@ const postsSchema = new Schema(
 			type: Schema.Types.ObjectId,
 			required: true,
 		},
+		title: {
+			type: String,
+			required: false,
+		},
 		text: {
 			type: String,
+			required: true,
+		},
+		isReply: {
+			type: Boolean,
 			required: true,
 		},
 		replies: {
 			type: [Schema.Types.ObjectId],
 			required: false,
 		},
+		reactions: {
+			type: [Schema.Types.ObjectId],
+			required: false,
+		}
 	},
 	{
 		timestamps: true,
