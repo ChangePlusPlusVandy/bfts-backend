40f70c36d9c85445bb9a8fc96188137647c5f739
Abi Kothapalli <abi.kothapalli@gmail.com>
2022-12-06 18:42:49 -0600

    Add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
 
 const app = express();
 
+app.use(cors());
 app.use(helmet());
 
 const port = 3000;
