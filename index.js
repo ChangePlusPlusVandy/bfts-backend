81ff91cea272cb8b8593857a03c07cfbd521d154
panda3141592 <39628281+panda3141592@users.noreply.github.com>
2022-12-15 21:43:02 +0700

    set up firebase

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const VerifyToken = require("./firebase-express-auth/VerifyToken");
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors()); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/data", VerifyToken, require("./firebase-express-auth/dataRoute"));
+
+app.get("/", (req, res) => {
+    res.send("Express Auth Temp!");
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
