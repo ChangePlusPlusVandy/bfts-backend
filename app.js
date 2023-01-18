017e09f62f38e2d65fe037c9171da7c1b1f13449
Abi Kothapalli <abi.kothapalli@gmail.com>
2023-01-17 19:21:38 -0600

    Setup dotenv

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+	console.log(`Example app listening on port ${PORT}`);
 });
