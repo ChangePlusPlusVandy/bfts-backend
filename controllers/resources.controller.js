387fca152954faa9e3c3497cb4fa3823d297e0b9
Abi Kothapalli <abi.kothapalli@gmail.com>
2023-01-24 20:59:20 -0600

    Implement endpoints

--- controllers/resources.controller.js
+++ controllers/resources.controller.js
@@ -1,19 +1,51 @@
 const Resource = require('../models/resources.js');
 
-const createResource = (req, res) => {
-	res.send('Create Resource');
+const createResource = async (req, res) => {
+	const data = new Resource({
+		title: req.body.title,
+		category: req.body.category,
+		website: req.body.website,
+		contactName: req.body.contactName,
+		contactNumber: req.body.contactNumber,
+	});
+
+	try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(500).json({message: error.message})
+    }
 };
 
-const deleteResource = (req, res) => {
-	res.send('Delete Resource');
+const deleteResource = async (req, res) => {
+	try{
+		const data = Resource.findByIdAndDelete(req.body.id);
+		res.json(data);
+	}
+	catch(error){
+		res.status(500).json({message: error.message})
+	}
 };
 
-const getResource = (req, res) => {
-	res.send('Get Resource');
+const getResource = async (req, res) => {
+	try{
+        const data = await Resource.findById(req.params.id);
+        res.json(data);
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 };
 
 const updateResource = (req, res) => {
-	res.send('Update Resource');
+	try{
+		const data = Resource.findByIdAndUpdate(req.body.id, req.body);
+		res.json(data);
+	}
+	catch(error){
+		res.status(500).json({message: error.message})
+	}
 };
 
 module.exports = {
