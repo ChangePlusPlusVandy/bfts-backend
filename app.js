c1f93cb141dbdef4a459da3f1ee47162d94f4799
Kyler Kang <kyler0809@gmail.com>
2023-01-30 23:27:45 -0600

    add to app.js

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use('/data', VerifyToken, require('./firebase-express-auth/dataRoute'));
 
 // Routers
 app.use('/messages', require('./routes/messages.router.js'));
+app.use('/volunteers', require('./routes/volunteers.router.js'));
+app.use('/clients', require('./routes/clients.router.js'));
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
