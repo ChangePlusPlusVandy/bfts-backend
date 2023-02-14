64bce7b98a955656d87e145473b813264029be56
Umphaipun Atipunumphai <panda3141592@gmail.com>
2023-02-14 15:22:46 -0600

    message mongo

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,21 +1,52 @@
-//const mongoose = require("mongoose");
-
+const mongoose = require("mongoose");
 const Message = require('../models/messages.js');
 
-const createMessage = (req, res) => {
-	res.send('Add a Message');
+const createMessage = async (req, res) => {
+	const message = new Message({
+		message: req.body.message,
+		sender: mongoose.Types.ObjectId(req.body.sender),
+	});
+
+	try {
+		const dataToSave = await message.save();
+		res.status(200).json(dataToSave);
+	} catch (error) {
+        res.status(500).json({message: error.message})
+    }
 };
 
-const getMessage = (req, res) => {
-	res.send('Get All Messages from user ' + req.params.userId);
+const getMessage = async (req, res) => {
+	try{
+        const post = await Message.find({sender : req.params.userId});
+        res.json(post)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 };
 
-const updateMessage = (req, res) => {
-	res.send('Update Message ' + req.params.messageId);
+const updateMessage = async (req, res) => {
+	try {
+		const filter = { _id: mongoose.Types.ObjectId(req.params.messageId) };
+		const targetPost = await Message.findOneAndUpdate(filter, {
+			message: req.body.message
+		});
+		res.send(targetPost);
+	}
+	catch (error) {
+        res.status(500).json({message: error.message})
+	}
 };
 
-const deleteMessage = (req, res) => {
-	res.send('Delete Message ' + req.params.messageId);
+const deleteMessage = async (req, res) => {
+	try {
+		const filter = { _id: mongoose.Types.ObjectId(req.params.messageId)};
+		const targetPost = await Message.deleteOne(filter);
+		res.send(targetPost);
+	}
+	catch (error) {
+		res.status(500).json({message: error.message})
+	}
 };
 
 module.exports = {
