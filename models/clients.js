6d252209a0397b1198247c960e17f2b97dbdfb00
Kyler Kang <kyler0809@gmail.com>
2023-02-14 20:08:03 -0600

    interacted schema added

--- models/clients.js
+++ models/clients.js
@@ -1,41 +1,55 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const clientsSchema = new Schema(
-	{
-		name: {
-			required: true,
-			type: String,
-		},
-		pronouns: {
-			required: true,
-			type: String,
-		},
-		birthday: {
-			required: true,
-			type: Number,
-		},
-		location: {
-			required: true,
-			type: String,
-		},
-		livingSituation: {
-			required: true,
-			type: String,
-		},
-		background: {
-			required: true,
-			type: String,
-		},
-		resourceTags: {
-			required: false,
-			type: [Schema.Types.ObjectId],
-		},
+const iteractedSchema = new Schema({
+	volunteer: {
+		required: true,
+		type: String,
 	},
-	{
-		timestamps: true,
-	}
-);
+	time: {
+		required: true,
+		type: Number,
+	},
+});
+
+const interacted = mongoose.model('interacted', interactedSchema);
+
+const clientsSchema = new Schema({
+	name: {
+		required: true,
+		type: String,
+	},
+	pronouns: {
+		required: true,
+		type: String,
+	},
+	birthday: {
+		required: true,
+		type: Number,
+	},
+	location: {
+		required: true,
+		type: String,
+	},
+	livingSituation: {
+		required: true,
+		type: String,
+	},
+	background: {
+		required: true,
+		type: String,
+	},
+	resourceTags: {
+		required: false,
+		type: [Schema.Types.ObjectId],
+	},
+	interacted: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'interacted',
+		},
+	],
+});
 
 // Prevent recompilation of model if it already exists
 module.exports = Message = mongoose.model('clients', clientsSchema);
