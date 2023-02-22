98187b9840c659f010637e5219bd577cd1b12a02
Umphaipun Atipunumphai <panda3141592@gmail.com>
2023-02-21 19:58:58 -0600

    add timesstamp to reply

--- models/posts.js
+++ models/posts.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 
-const repliesSchema = new mongoose.Schema({
-	poster: {
-		type: String,
-		required: true,
+const repliesSchema = new mongoose.Schema(
+	{
+		poster: {
+			type: String,
+			required: true,
+		},
+		text: {
+			type: String,
+			required: false,
+		},
 	},
-	text: {
-		type: String,
-		required: false,
-	},
-});
-	
+	{
+		timestamps: true,
+	}
+);
+
 const postSchema = new mongoose.Schema(
 	{
 		poster: {
