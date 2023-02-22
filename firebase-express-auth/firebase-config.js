c0b7540475d04ec80237fc34bbc38e16d9cfb9f4
abi-kothapalli <abi-kothapalli@users.noreply.github.com>
2023-02-22 02:00:51 +0000

    Run Prettier

--- firebase-express-auth/firebase-config.js
+++ firebase-express-auth/firebase-config.js
@@ -7,13 +7,13 @@ dotenv.config({
 });
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-  };
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 
