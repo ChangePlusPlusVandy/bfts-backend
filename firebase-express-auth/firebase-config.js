2dac1c06b78848bdf854bd1a594be930fcd38449
Umphaipun Atipunumphai <panda3141592@gmail.com>
2023-04-04 19:22:18 -0500

    auth

--- firebase-express-auth/firebase-config.js
+++ firebase-express-auth/firebase-config.js
@@ -1,18 +1,14 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getAuth } = require('firebase-admin/auth');
 const dotenv = require('dotenv');
+const serviceAccount = require('./serviceAccountKey.json');
 
 dotenv.config({
 	path: '../config.env',
 });
 
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
+	credential: cert(serviceAccount),
 };
 
 const app = initializeApp(firebaseConfig);
