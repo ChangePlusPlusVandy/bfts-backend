6e80ea8ebae4bb2f9d2018ea404ca00655214cbf
Abi Kothapalli <abi.kothapalli@gmail.com>
2023-04-04 19:18:59 -0500

    Add auth stuff

--- firebase-express-auth/firebase-config.js
+++ firebase-express-auth/firebase-config.js
@@ -2,20 +2,11 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getAuth } = require('firebase-admin/auth');
 const dotenv = require('dotenv');
 
-dotenv.config({
-	path: '../config.env',
+const serviceAccountKey = require("./service-account-key.json");
+
+const app = initializeApp({
+  credential: cert(serviceAccountKey),
 });
-
-const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-};
-
-const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
