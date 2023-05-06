e5f77400e9ad5bb38ec2d26d3557f2de88b917b0
svohra02 <66923911+svohra02@users.noreply.github.com>
2023-05-06 19:50:08 -0400

    updated volunteer finds

--- controllers/volunteers.controllers.js
+++ controllers/volunteers.controllers.js
@@ -28,7 +28,7 @@ const createVolunteer = async (req, res) => {
 
 const deleteVolunteer = async (req, res) => {
 	try {
-		const filter = { _id: mongoose.Types.ObjectId(req.body.volunteerId) };
+		const filter = { _id: mongoose.Types.ObjectId(req.params.volunteerId) };
 		const targetPost = await Volunteer.deleteOne(filter);
 		res.send(targetPost);
 	} catch (error) {
@@ -38,7 +38,7 @@ const deleteVolunteer = async (req, res) => {
 
 const getVolunteer = async (req, res) => {
 	try {
-		const filter = {  _id: mongoose.Types.ObjectId(req.body.volunteerId) };
+		const filter = { _id: mongoose.Types.ObjectId(req.params.volunteerId) };
 		const data = await Volunteer.findOne(filter);
 		res.json(data);
 	} catch (error) {
